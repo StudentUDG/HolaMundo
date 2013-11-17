58e9ded40ca8c7addad562b641dc811a52a692f1
nestor41 <nestor_infante@hotmail.com>
2013-11-16 19:28:09 -0600

    Finalizado!!!

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HolaMundo
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hola mundo!!!!");
+			Console.ReadKey();
+		}
+	}
+}
